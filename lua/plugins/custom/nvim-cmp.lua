9a685a1256b1349b3026febd075ec561ec28fb33
Semen Tonkikh <tonksemen@gmail.com>
2025-10-03 19:00:27 +0300

    refactor(nvim-cmp): snippets works now!

--- lua/plugins/custom/nvim-cmp.lua
+++ lua/plugins/custom/nvim-cmp.lua
@@ -11,20 +11,45 @@ return {
   -- event = "VeryLazy",
   config = function()
     local cmp = require "cmp"
+    local luasnip = require "luasnip"
+
+    require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_vscode").lazy_load {
+      paths = { vim.fn.stdpath "config" .. "/lua/snippets" },
+    }
+
     cmp.setup {
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       },
       mapping = {
-        ["<CR>"] = nil, -- Enter отключен
+        ["<C-y>"] = cmp.mapping.confirm { select = true },
+        ["<CR>"] = nil,
         ["<Tab>"] = nil,
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-p>"] = cmp.mapping.select_prev_item(),
+
+        ["<C-f>"] = cmp.mapping(function()
+          if luasnip.jumpable(1) then
+            luasnip.jump(1)
+          end
+        end, { "i", "s" }),
+        ["<C-b>"] = cmp.mapping(function()
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { "i", "s" }),
       },
       sources = cmp.config.sources {
         { name = "codeium" },
         { name = "nvim_lsp" },
+        { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
       },
