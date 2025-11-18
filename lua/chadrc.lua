d4e7c6326e5454e8acdd8a7107b22b1cd0a8800b
Semen Tonkikh <tonksemen@gmail.com>
2025-11-18 15:54:25 +0300

    upd chardrc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -12,10 +12,10 @@ M.base46 = {
   integrations = {
     "render-markdown",
   },
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+  hl_override = {
+  	Comment = { italic = true },
+  	["@comment"] = { italic = true },
+  },
 }
 
 M.nvdash = { load_on_startup = true }
