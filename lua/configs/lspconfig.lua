7cb2d7c61ecbe153f9b14fa655e868a667dae902
Semen Tonkikh <tonksemen@gmail.com>
2025-11-28 15:22:08 +0300

    remove complitions from tailwindcss lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -23,4 +23,17 @@ vim.lsp.config["cssls"] = {
 local servers = { "html", "cssls", "vtsls", "jsonls", "tailwindcss" }
 vim.lsp.enable(servers)
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then
+      return
+    end
+
+    if client.name == "tailwindcss" then
+      -- чтобы nvim-cmp не получал подсказки от tailwind LSP
+      client.server_capabilities.completionProvider = nil
+    end
+  end,
+})
 -- read :h vim.lsp.config for changing options of lsp servers
