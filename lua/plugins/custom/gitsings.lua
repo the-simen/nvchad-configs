424bd0d8ea28bab4846fcfbafabfc4b3188e219a
Tonkikh Semen <tonksemen@gmail.com>
2026-05-26 02:10:49 +0300

    upd

--- lua/plugins/custom/gitsings.lua
+++ lua/plugins/custom/gitsings.lua
@@ -16,5 +16,14 @@ return {
       follow_files = true,
     },
     auto_attach = true,
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      delay = 500,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+      use_focus = true,
+    },
   },
 }
